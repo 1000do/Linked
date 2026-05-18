a6d9822bd671553848a53ba8066903c00b0478cd
AKER\anhkc <anhk.ce191266@gmail.com>
2026-05-18 21:39:47 +0700

    Rename AiModelType -> AiModelConst, add more consts

---
+++ CourseMarketplaceBE/CourseMarketplaceBE/Domain/Constants/AiModelConst.cs
@@ -0,0 +1,11 @@
+namespace CourseMarketplaceBE.Domain.Constants
+{
+    public static class AiModelConst
+    {
+        public const string Moderator = "moderator";
+        public const string Generator = "generator";
+        public const string Classifier = "classifier";
+        public const string Active = "active";
+        public const string Inactive = "inactive";
+    }
+}
--- CourseMarketplaceBE/CourseMarketplaceBE/Domain/Constants/AiModelType.cs
+++
@@ -1,8 +0,0 @@
-namespace CourseMarketplaceBE.Domain.Constants
-{
-    public static class AiModelType
-    {
-        public const string Moderator = "moderator";
-        public const string Generator = "generator";
-    }
-}
